fe232ffbb9e3558ca2edb91e73549b18083186c8
fadjar setiawan <fadjarsetiawans@gmail.com>
2021-03-12 14:24:24 +0700

    edit contact

--- contact2.php
+++ contact2.php
@@ -47,7 +47,7 @@ include("atas.php");
         <div class="row" data-aos="fade-up" data-aos-delay="100">
 
           <div class="col-lg-20 ">
-            <iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 400px;" allowfullscreen></iframe>
+            <iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d126804.62235511957!2d108.4984625!3d-6.690886!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x766d61f225a71a5f!2sKakau!5e0!3m2!1sid!2sid!4v1615533820330!5m2!1sid!2sid" frameborder="0" style="border:0; width: 100%; height: 400px;" allowfullscreen></iframe>
         </div>
 
       </div>
